fa482155e0b34204b93ef9b49bde03d019550425
high <highcloud100@inha.edu>
2023-11-09 20:56:12 +0900

    precommit adopted

--- fastApi/main.py
+++ fastApi/main.py
@@ -1,19 +1,22 @@
-from fastapi import FastAPI
 import openai
+
+from fastapi import FastAPI
 from fastApi.setting import Settings
 
 
 app = FastAPI()
 
+
 @app.get("/items/{item_id}")
-async def test(item_id:int):
-    return {"message" : "Hellasdfasdfasdfasdfo World",
-            "item_id" : item_id}
+async def test(item_id: int):
+    return {"message": "Hellasdfasdfasdfasdfo World", "item_id": item_id}
+
 
 @app.get("/prompt/{prompt}")
-async def prompt(prompt:str):
+async def prompt(prompt: str):
     openai.api_key = "empty"
     openai.api_base = Settings().llm_server_URL
-    completion = openai.Completion.create(model="lmsys/vicuna-7b-v1.3", prompt=prompt, max_tokens=30)
-    return {"message" : completion.choices[0].text}
-
+    completion = openai.Completion.create(
+        model="lmsys/vicuna-7b-v1.3", prompt=prompt, max_tokens=30
+    )
+    return {"message": completion.choices[0].text}
--- fastApi/setting.py
+++ fastApi/setting.py
@@ -1,10 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from functools import lru_cache
 
 
 class Settings(BaseSettings):
     llm_server_URL: str
 
     model_config = SettingsConfigDict(env_file="/app/fastApi/.env")
-
-
