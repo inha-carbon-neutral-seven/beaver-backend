1776826142e62c54bb0021aaac6f35cb0dd32741
high <highcloud100@inha.edu>
2023-11-14 22:16:49 +0900

    Refactor GitHub workflow and make.sh script

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 container_name=fastApi
 image_name=biber
 docker stop $container_name
@@ -11,6 +12,10 @@ docker builder builder prune # for cache
 docker build -t $image_name .
 docker run -d --name $container_name -p 10100:10100 $image_name
 
-## testing
-img_id=$(docker ps | grep biber | awk '{print $1}')
-docker exec -i $img_id /bin/bash -c "pytest"
+
+if ["$1" = "build"]
+then
+    ## testing
+    img_id=$(docker ps | grep biber | awk '{print $1}')
+    docker exec -i $img_id /bin/bash -c "pytest"
+fi
