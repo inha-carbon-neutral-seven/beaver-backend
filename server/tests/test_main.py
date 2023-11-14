c9074bec12b5871219b86960b7964da0f93f4e9e
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-11-14 17:53:42 +0900

    refactor: tests 분리

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -1,12 +1,15 @@
 from fastapi.testclient import TestClient
 
-from .main import app
-
+from ..main import app
 
 client = TestClient(app)
 
+"""
+테스트 케이스 작성 시 함수 이름을 test_ 로 지어야 합니다.
+"""
 
-def 모델_서버_상태_확인():
+
+def test_모델_서버_상태_확인():
     response = client.get("/ping")
     assert response.status_code == 200
     assert "status" in response.json()
