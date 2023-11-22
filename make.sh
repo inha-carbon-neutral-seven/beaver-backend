877733b06fccab9675d4396355dc21ff8e087193
highcloud100 <80192345+highcloud100@users.noreply.github.com>
2023-11-22 20:35:33 +0900

    Update make.sh

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ docker stop $container_name
 docker rm $container_name
 docker rmi $image_name
 
-docker builder builder prune # for cache
+#docker builder builder prune # for cache
 
 docker build -t $image_name .
 docker run -d --name $container_name -p 10100:10100 $image_name
@@ -15,4 +15,4 @@ docker run -d --name $container_name -p 10100:10100 $image_name
 
 ## testing
 img_id=$(docker ps | grep biber | awk '{print $1}')
-docker exec -i $img_id /bin/bash -c "pytest"
+docker exec -i $img_id /bin/bash -c "cd server && pytest"
