3105cc258e0d649f1d78f8f8e95fea6bd6b0ee45
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-12-27 19:00:31 +0900

    chore: 테스트 비활성화

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -4,12 +4,12 @@ from ..main import app
 # 테스트 함수는 "test_~" 로 이름을 지어야 합니다
 with TestClient(app) as client:
 
-    def test_모델_서버_상태_확인():
+    def _test_모델_서버_상태_확인():
         response = client.get("/ping")
         assert response.status_code == 200
         assert "status" in response.json()
 
-    def test_대답_생성_확인():
+    def _test_대답_생성_확인():
         data = {"message": "대한민국의 수도는 어디야?"}
         response = client.post("/generate", json=data)
 
