fada1bd0c817f621893c354a08f2b0bf8bbbbee8
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2024-02-11 21:42:20 +0900

    Refactor OutputParser(langchain) to Synthesizer(llamaindex) for accurate output form

--- server/services/chains/recap.py
+++ server/services/chains/recap.py
@@ -1,9 +1,9 @@
 import logging
-from llama_index import ServiceContext
-from llama_index.output_parsers import LangchainOutputParser
+from llama_index import ServiceContext, SimpleDirectoryReader
 from llama_index.llms import OpenAI
-from server.services.storage import load_index
-from server.services.output_parsers.output_parsers import RecapOutput, recap_parser
+from llama_index.response_synthesizers import TreeSummarize
+from server.services.storage import get_document_path
+from server.models.recap import RecapOutput
 
 
 def generate_recap() -> RecapOutput:
@@ -19,28 +19,29 @@ def generate_recap() -> RecapOutput:
     """
     logging.info("recap chain 실행 ...")
 
-    query_message = """Create a Recap according to the instructions in Korean.
-Fill in the summary part abundantly."""
+    document_path = get_document_path()
+    documents = SimpleDirectoryReader(document_path).load_data()
 
-    index = load_index()
+    document_text = documents[0].text
+
     service_context = _load_service_context()
 
-    engine = index.as_query_engine(response_mode="tree_summarize", service_context=service_context)
+    summarizer = TreeSummarize(
+        service_context=service_context,
+        output_cls=RecapOutput,
+        verbose=True,
+    )
 
-    res = engine.query(query_message)
+    query_message = "Create a Recap in Korean. Fill in the summary part abundantly."
 
-    output_text = res.response
+    res = summarizer.get_response(query_str=query_message, text_chunks=[document_text])
+    logging.info("recap result: %s", res.to_dict())
 
-    recap_output = recap_parser.parse(output_text)
-    print(f"{recap_output}")
-    return recap_output
+    return res
 
 
 def _load_service_context():
 
-    # define output parser
-    output_parser = LangchainOutputParser(recap_parser)
-
-    llm = OpenAI(model="gpt-3.5-turbo-0125", output_parser=output_parser)
+    llm = OpenAI(model="gpt-3.5-turbo-0125")
     service_context = ServiceContext.from_defaults(llm=llm)
     return service_context
